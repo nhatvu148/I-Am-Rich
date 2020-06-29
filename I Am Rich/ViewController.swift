f28be2ca9e7f51e00f992aea696bd20a9dc590c0
NHAT VU <nhatvu148@NHATs-MacBook-Pro.local>
2020-06-29 22:56:38 +0900

    initial commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by NHAT VU on 6/29/20.
+//  Copyright © 2020 NHAT VU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
